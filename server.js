48c6224926b062ddc3a68958a576a69c1ab7d2be
Jasmine Terry <jasmine.terry91@gmail.com>
2020-02-12 14:14:33 -0500

    end of lecture push

--- server.js
+++ server.js
@@ -1,20 +1,55 @@
-const express = require('express'); // importing a CommonJS module
+const express = require("express"); // importing a CommonJS module
+const morgan = require("morgan");
+const helmet = require("helmet");
 
-const hubsRouter = require('./hubs/hubs-router.js');
+const hubsRouter = require("./hubs/hubs-router.js");
 
 const server = express();
 
-server.use(express.json());
+// the three amigas: rachel, rita and nancy
 
-server.use('/api/hubs', hubsRouter);
+// global middleware
+server.use(express.json()); // built-in middleware
+// server.use(morgan("dev")); //another middleware imported globally
+server.use(helmet());
+// server.use(logger); //to use globally
 
-server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+// routes - endpoints 
+server.use("/api/hubs", logger, gatekeeper("mellon"), hubsRouter);
 
+server.get("/", logger, greeter, gatekeeper("notto"), (req, res) => {
   res.send(`
     <h2>Lambda Hubs API</h2>
-    <p>Welcome${nameInsert} to the Lambda Hubs API</p>
+    <p>Welcome ${req.cohort} to the Lambda Hubs API</p>
     `);
 });
 
 module.exports = server;
+
+function greeter(req, res, next) {
+  req.cohort = "Web 26";
+
+  next();
+}
+
+function logger(req, res, next) {
+  console.log(`${req.method} Request to ${req.originalUrl} `);
+
+  next();
+}
+
+function gatekeeper(guess) {
+  return function(req, res, nancy) {
+    const password = req.headers.password;
+
+    console.log("gk headers", req.headers);
+
+    if (password && password.toLowerCase() === guess) {
+      nancy();
+    } else {
+      res.status(401).json({ you: "shall not pass!" });
+    }
+  };
+}
+
+
